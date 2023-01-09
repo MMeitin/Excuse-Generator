b9398c4e830e428ccd6f254422970004ea1a4d56
MMeitin <mario.meitin.alvarez@gmail.com>
2023-01-09 20:17:45 +0000

    Add excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = generarExcusa();
+  function generarExcusa() {
+    let sujeto = [
+      `Marisa`,
+      `Mi abuelo`,
+      `La tía Luisa`,
+      `El repartidor de Correos`
+    ];
+    let nucleo = [`rompió`, `deshizo`, `perdió`, `extravió`];
+    let predicado = [`el regalo`, `mi llave`, `mis deberes`, `el cuadro`];
+    let complemento = [`ayer`, `hoy`, `a la mañana`, `hace 5 minutos`];
+
+    return (
+      sujeto[Math.floor(Math.random() * sujeto.length)] +
+      ` ` +
+      nucleo[Math.floor(Math.random() * nucleo.length)] +
+      ` ` +
+      predicado[Math.floor(Math.random() * predicado.length)] +
+      ` ` +
+      complemento[Math.floor(Math.random() * complemento.length)]
+    );
+  }
 };
