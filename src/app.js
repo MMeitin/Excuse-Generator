3f61edd8a5ee3d48f02b74c49abcc80fdbf82bd0
MMeitin <mario.meitin.alvarez@gmail.com>
2023-01-09 20:59:44 +0000

    Add button generator

--- src/app.js
+++ src/app.js
@@ -6,26 +6,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
-  document.querySelector("#excusa").innerHTML = generarExcusa();
-  function generarExcusa() {
-    let sujeto = [
-      `Marisa`,
-      `Mi abuelo`,
-      `La tía Luisa`,
-      `El repartidor de Correos`
-    ];
-    let nucleo = [`rompió`, `deshizo`, `perdió`, `extravió`];
-    let predicado = [`el regalo`, `mi llave`, `mis deberes`, `el cuadro`];
-    let complemento = [`ayer`, `hoy`, `a la mañana`, `hace 5 minutos`];
+  document.querySelector("#btnExcusa").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generarExcusa();
+  });
+};
+let generarExcusa = () => {
+  let sujeto = [
+    `Marisa`,
+    `Mi abuelo`,
+    `La tía Luisa`,
+    `El repartidor de Correos`
+  ];
+  let nucleo = [`rompió`, `deshizo`, `perdió`, `extravió`];
+  let predicado = [`el regalo`, `mi llave`, `mis deberes`, `el cuadro`];
+  let complemento = [`ayer`, `hoy`, `a la mañana`, `hace 5 minutos`];
 
-    return (
-      sujeto[Math.floor(Math.random() * sujeto.length)] +
-      ` ` +
-      nucleo[Math.floor(Math.random() * nucleo.length)] +
-      ` ` +
-      predicado[Math.floor(Math.random() * predicado.length)] +
-      ` ` +
-      complemento[Math.floor(Math.random() * complemento.length)]
-    );
-  }
+  return (
+    sujeto[Math.floor(Math.random() * sujeto.length)] +
+    ` ` +
+    nucleo[Math.floor(Math.random() * nucleo.length)] +
+    ` ` +
+    predicado[Math.floor(Math.random() * predicado.length)] +
+    ` ` +
+    complemento[Math.floor(Math.random() * complemento.length)]
+  );
 };
